8c2149c26e850e630b54e0eece33a16bd72eb59c
adamluffy <adam.mourin@hotmail.com>
2023-09-24 15:29:55 +0800

    Extract delete modal to a template

---
+++ resources/views/delete-modal.blade.php
@@ -0,0 +1,22 @@
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+
+        <div class="modal-header">
+          <h5 class="modal-title">Confirm Delete</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+
+        <div class="modal-body">
+          <p>Are you sure u want to delete this item?</p>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-danger" id="delete-product">Delete</button>
+        </div>
+
+      </div>
+
+    </div>
+</div>
--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -2,7 +2,6 @@
 
 @section("title", "Product List")
 
-
 @section("content")
 
     <div class="d-flex">
@@ -55,28 +54,7 @@
         </tbody>
     </table>
 
-    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-          <div class="modal-content">
-
-            <div class="modal-header">
-              <h5 class="modal-title">Confirm Delete</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <div class="modal-body">
-              <p>Are you sure u want to delete this item?</p>
-            </div>
-
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-danger" id="delete-product">Delete</button>
-            </div>
-
-          </div>
-
-        </div>
-    </div>
+    @include("delete-modal")
 
     @vite('resources/ts/product/delete-product.ts')
 @endsection
